2f8e30fa93c8d53cce617ce87c8a5fd9120da5cb
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2021-04-23 15:29:05 -0400

    downloaded necessary packages

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="Landing">
+        <div className="canvas">
 
+        </div>
+
+        <h1>Hello Portfolio</h1>
+      </div>
     </div>
   );
 }
